0ed087d58d2c6c3e3d1caf07144bff56316d7ead
Andreas Kruger <ak@pasientsky.no>
2016-02-24 13:40:51 +0100

    Added pool ping check and increased pool

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ var knex = require('knex')(
     database : (process.env.MYSQL_DB || mysql_config.get('database') || 'asterisk')
   },
   pool: {
+      ping: function(connection, callback) {
+          connection.query({sql: 'SELECT 1 = 1'}, [], callback);
+      },
+      pingTimeout: 3*1000,
       min: 1,
-      max: 1
+      max: 2
   }
 });
 
