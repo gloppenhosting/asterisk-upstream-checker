b40a6f0f8cfb830ee176d1ce4cacc665a8e960eb
Andreas Kruger <ak@pasientsky.no>
2016-06-30 08:42:41 +0200

    Create contacts view

--- server.js
+++ server.js
@@ -67,13 +67,16 @@ domain.run(function() {
     var create_table_query2 = "CREATE VIEW " + view_name2 + " AS SELECT ps_registrations.* FROM ps_endpoints_has_iaxfriends INNER JOIN ps_registrations ON ps_registrations.id = ps_endpoints_has_iaxfriends.ps_endpoints_id INNER JOIN iaxfriends ON ps_endpoints_has_iaxfriends.iaxfriends_id = iaxfriends.id WHERE iaxfriends.name = '" + os.hostname() + "'";
 
     var view_name3 = "ps_aors_" + md5(os.hostname());
-    var create_table_query3 = "CREATE VIEW " + view_name3 + " AS SELECT ps_aors.* FROM ps_aors INNER JOIN ps_endpoints AS Z ON Z.aors = ps_aors.id INNER JOIN ps_endpoints_has_iaxfriends AS X ON X.ps_endpoints_id = Z.id  INNER JOIN iaxfriends AS Y ON X.iaxfriends_id = Y.id WHERE Z.context = 'external' AND Y.name = '" + os.hostname() + "'";
+    var create_table_query3 = "CREATE VIEW " + view_name3 + " AS SELECT ps_aors.* FROM ps_aors INNER JOIN ps_endpoints AS Z ON Z.aors = ps_aors.id INNER JOIN ps_endpoints_has_iaxfriends AS X ON X.ps_endpoints_id = Z.id INNER JOIN iaxfriends AS Y ON X.iaxfriends_id = Y.id WHERE Z.context = 'external' AND Y.name = '" + os.hostname() + "'";
 
     var view_endpoint_internal = "ps_endpoints_internal";
     var create_table_endpoint_internal = "CREATE VIEW " + view_endpoint_internal + " AS SELECT ps_endpoints.* FROM ps_endpoints WHERE context = 'internal'";
 
     var view_aors_internal = "ps_aors_internal";
     var create_table_aors_internal = "CREATE VIEW " + view_aors_internal + " AS SELECT ps_aors.* FROM ps_aors INNER JOIN ps_endpoints ON ps_aors.id = ps_endpoints.aors WHERE ps_endpoints.context = 'internal'";
+
+    var view_contacts_internal = "ps_contacts_" + md5(os.hostname());
+    var create_table_contacts_internal = "CREATE VIEW " + view_contacts_internal + " AS SELECT ps_contacts.* FROM ps_contacts INNER JOIN iaxfriends ON ps_contacts.regserver = iaxfriends.ipaddr WHERE iaxfriends.hostname = '" + os.hostname() + "'";
 
     // Check if view for this upstream is in the database
     var check_for_view = function() {
@@ -189,6 +192,28 @@ domain.run(function() {
                     .catch(function(error) {
                         if (debug) {
                             console.log(moment(new Date()).format("YYYY-MM-DD HH:mm:ss"), 'Unable to create view for ps_aors internal', view_aors_internal, error);
+                        }
+                    });
+            });
+
+        knex.select('id')
+            .from(view_contacts_internal)
+            .catch(function(error) {
+                // Create view as it's missing
+                knex.transaction(function(trx) {
+                        trx
+                            .raw(create_table_contacts_internal)
+                            .then(trx.commit)
+                            .catch(trx.rollback);
+                    })
+                    .then(function(resp) {
+                        if (debug) {
+                            console.log(moment(new Date()).format("YYYY-MM-DD HH:mm:ss"), 'Created view for ps_contacts', view_contacts_internal);
+                        }
+                    })
+                    .catch(function(error) {
+                        if (debug) {
+                            console.log(moment(new Date()).format("YYYY-MM-DD HH:mm:ss"), 'Unable to create view for ps_contacts', view_contacts_internal, error);
                         }
                     });
             });
